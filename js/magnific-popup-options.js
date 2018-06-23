1a2ca3de99dd4822f5cd6e755bae1d95aa781ab3
jfreek <jorgee.abud@gmail.com>
2018-06-22 22:25:11 -0600

    logo size changed, gallery done

--- js/magnific-popup-options.js
+++ js/magnific-popup-options.js
@@ -53,3 +53,21 @@ $(document).ready(function() {
 	magnifVideo();
 
 });
+
+$(document).ready(function() {
+
+	$('a.btn-gallery').on('click', function(event) {
+		event.preventDefault();
+		
+		var gallery = $(this).attr('href');
+    
+		$(gallery).magnificPopup({
+      delegate: 'a',
+			type:'image',
+			gallery: {
+				enabled: true
+			}
+		}).magnificPopup('open');
+	});
+	
+});
